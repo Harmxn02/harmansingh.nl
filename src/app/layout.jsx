8a97af0b19206f94bf64cb94c4553e26ce2fece1
Harman Singh <harman.singh@student.howest.be>
2024-02-01 20:41:32 +0100

    Add Vercel Speed Insights

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -17,6 +18,7 @@ export default function RootLayout({ children }) {
 			<body className={inter.className}>
 				{children}
 				<Analytics />
+				<SpeedInsights  />
 			</body>
 		</html>
 	);
