b22a262810622df8af62c4112e10539aa7f9df3d
Harman Singh <harman.singh@student.howest.be>
2024-02-01 20:38:36 +0100

    Add back Vercel Analytics

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import { Analytics } from "@vercel/analytics/react";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 
@@ -12,7 +14,10 @@ export default function RootLayout({ children }) {
 	return (
 		// you can remove `className={inter.className}`, and it will look exactly the same as the old version
 		<html lang="en">
-			<body className={inter.className}>{children}</body>
+			<body className={inter.className}>
+				{children}
+				<Analytics />
+			</body>
 		</html>
 	);
 }
