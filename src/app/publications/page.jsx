6fdfbe0a55ab7619601558caa1075b201f87084d
Harman Singh <harman.singh@student.howest.be>
2024-02-06 23:14:42 +0100

    Add dropdown button component for selecting categories

--- src/app/publications/page.jsx
+++ src/app/publications/page.jsx
@@ -6,6 +6,87 @@ import Publication from "./Publication";
 
 import Header from "./Header";
 
+const DropdownButton = ({
+	activeSelection,
+	handleCategoryClick,
+	articlesPerCategory,
+	text,
+}) => {
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setIsDropdownOpen(!isDropdownOpen);
+	};
+
+	return (
+		<div className="relative inline-block text-left">
+			<button
+				type="button"
+				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				onClick={toggleDropdown}
+			>
+				{text}{" "}
+				<svg
+					className="w-2.5 h-2.5 ms-3"
+					aria-hidden="true"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 10 6"
+				>
+					<path
+						stroke="currentColor"
+						strokeLinecap="round"
+						strokeLinejoin="round"
+						strokeWidth="2"
+						d="m1 1 4 4 4-4"
+					/>
+				</svg>
+			</button>
+
+			{/* Dropdown menu */}
+			{isDropdownOpen && (
+				<div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 mt-2">
+					<ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
+						<li>
+							<a
+								href="#"
+								onClick={() => {
+									handleCategoryClick("");
+									toggleDropdown();
+								}}
+								className={`block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ${
+									activeSelection === "" ? "font-bold" : ""
+								}`}
+							>
+								All ({publications.length})
+							</a>
+						</li>
+						{Object.keys(articlesPerCategory).map((category) => (
+							<li key={category}>
+								<a
+									href="#"
+									onClick={() => {
+										handleCategoryClick(category);
+										toggleDropdown();
+									}}
+									className={`block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white ${
+										activeSelection === category
+											? "font-bold"
+											: ""
+									}`}
+								>
+									{category} ({articlesPerCategory[category]})
+								</a>
+							</li>
+						))}
+					</ul>
+				</div>
+			)}
+		</div>
+	);
+};
+
+//prettier-ignore
 const Page = () => {
 	const [selectedCategory, setSelectedCategory] = useState("");
 	const [activeSelection, setActiveSelection] = useState("");
@@ -56,9 +137,21 @@ const Page = () => {
 					})}
 				</div>
 
-				<div className="md:ml-10 border md:border-none p-2 md:p-0">
+				<div className="md:hidden">
+					<DropdownButton
+						
+						// change this to change what the button says
+						text="Select category"
+						
+						activeSelection={activeSelection}
+						handleCategoryClick={handleCategoryClick}
+						articlesPerCategory={articlesPerCategory}
+					/>
+				</div>
+
+				<div className="md:ml-10 hidden md:block">
 					<p className="font-bold my-2 ">Categories:</p>
-					<ul className="flex md:flex-col flex-wrap gap-3 md:gap-0">
+					<ul>
 						<li
 							onClick={() => handleCategoryClick("")}
 							className={
