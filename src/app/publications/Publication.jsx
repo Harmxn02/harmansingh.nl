2af7030e09a51e6279b96ae241af043ddb435230
Harman Singh <harman.singh@student.howest.be>
2024-02-06 23:56:24 +0100

    a lot of changes to the article URLs

--- src/app/publications/Publication.jsx
+++ src/app/publications/Publication.jsx
@@ -1,42 +1,54 @@
 import React from "react";
 
+import Link from "next/link";
+
 const formatDate = (dateString) => {
 	const [day, month, year] = dateString.split("-");
 	const parsedDate = new Date(`${year}-${month}-${day}T00:00:00Z`);
 
 	const options = { year: "numeric", month: "short", day: "numeric" };
-	const formattedDate = parsedDate.toLocaleDateString(
-		"en-US",
-		options
-	);
+	const formattedDate = parsedDate.toLocaleDateString("en-US", options);
 	return formattedDate;
+};
+
+const truncateDescription = (description) => {
+	const maxLength = 250;
+	if (description.length > maxLength) {
+		return description.slice(0, maxLength) + " ...";
+	}
+	return description;
 };
 
 const Publication = ({ publication }) => {
 	const formattedDate = formatDate(publication.date);
+	const truncatedDescription = truncateDescription(publication.description);
 
 	return (
 		<div className="flex items-baseline gap-4">
-
 			{/* Metadata - LEFT */} {/* shown after MD */}
 			<div key={publication.key} className="hidden lg:block text-xs">
 				<p>{formattedDate}</p>
 				<p>{publication.author}</p>
 			</div>
-
 			{/* Article */}
 			<div key={publication.key} className="border-b py-6">
-				<h1 className="font-bold text-xl">{publication.title}</h1>
-				<p className="font-serif max-w-prose">
-					{publication.description}
-				</p>
-
+				<h1 className="font-bold text-xl hover:cursor-pointer hover:underline">
+					{publication.URL ? (
+						<Link href={publication.URL} legacyBehavior passHref>
+							<a target="_blank" rel="noopener noreferrer">
+								{publication.title}
+							</a>
+						</Link>
+					) : (
+						<span>{publication.title}</span>
+					)}
+				</h1>
+				<p className="font-serif max-w-prose">{truncatedDescription}</p>
 				{/* Metadata - UNDER */} {/* shown before MD (mobile) */}
-				<div key={publication.key} className="lg:hidden pt-2 text-xs">
+				<div key={publication.key} className="lg:hidden pt-4 text-xs">
 					<p>{formattedDate}</p>
 					<p>{publication.author}</p>
 				</div>
-
 				<ul className="flex gap-1 mt-4">
 					{publication.themes.map((theme, index) => {
 						return (
--- src/app/publications/page.jsx
+++ src/app/publications/page.jsx
@@ -6,6 +6,16 @@ import Publication from "./Publication";
 
 import Header from "./Header";
 
+
+const truncateListElement = (text) => {
+	const maxLength = 14;
+	if (text.length > maxLength) {
+		return text.slice(0, maxLength) + " ...";
+	}
+	return text;
+}
+
+
 const DropdownButton = ({
 	activeSelection,
 	handleCategoryClick,
@@ -22,7 +32,7 @@ const DropdownButton = ({
 		<div className="relative inline-block text-left">
 			<button
 				type="button"
-				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+				className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center"
 				onClick={toggleDropdown}
 			>
 				{text}{" "}
@@ -125,7 +135,7 @@ const Page = () => {
 	return (
 		<div>
 			<Header />
-			<div className="flex flex-col-reverse md:flex-row m-10">
+			<div className="flex flex-col-reverse md:flex-row m-10 ">
 				<div className="max-w-3xl">
 					{filteredPublications.map((publication) => {
 						return (
@@ -149,7 +159,7 @@ const Page = () => {
 					/>
 				</div>
 
-				<div className="md:ml-10 hidden md:block">
+				<div className="md:ml-10 hidden md:block min-w-fit">
 					<p className="font-bold my-2 ">Categories:</p>
 					<ul>
 						<li
@@ -170,7 +180,7 @@ const Page = () => {
 										: "cursor-pointer"
 								}
 							>
-								{category} ({articlesPerCategory[category]})
+								{truncateListElement(category)} ({articlesPerCategory[category]})
 							</li>
 							
 						))}
