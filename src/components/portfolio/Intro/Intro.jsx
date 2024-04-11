5f49ced7358a0ee09945e1ab0a4b77ec3fc2c836
Harman Singh <harman.singh@student.howest.be>
2024-04-11 21:46:15 +0200

    UPDATE: logo's

--- src/components/portfolio/Intro/Intro.jsx
+++ src/components/portfolio/Intro/Intro.jsx
@@ -2,7 +2,7 @@
 import { Fragment } from "react";
 import { Popover, Transition } from "@headlessui/react";
 import { MenuIcon, XIcon } from "@heroicons/react/outline";
-import NAME from "../images/logos/NAME_WHITE.png";
+import NAME from "../images/logos/NEW_512.png";
 import robot from "./img/robot-purple-compressed.webp";
 import CTA from "./CTA";
 
