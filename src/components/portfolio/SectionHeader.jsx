7cb0b9344394a96ac68a9f016409ee82be24806a
Harman Singh <harman.singh@student.howest.be>
2024-07-22 20:41:42 +0200

    Change font of SectionHeader Change Hashtag to a purple period

--- src/components/portfolio/SectionHeader.jsx
+++ src/components/portfolio/SectionHeader.jsx
@@ -1,11 +1,11 @@
-const Hashtag = ({ href }) => {
+const Suffix = ({ href }) => {
 	return (
-		<span className="text-[#374151]">
+		<span className="text-[#9f4fd9]">
 			<a
-				className="hover:text-white px-4 transition-colors duration-500 "
+				className="hover:text-white px-1 transition-colors duration-500 "
 				href={href}
 			>
-				#
+				.
 			</a>
 		</span>
 	);
@@ -17,9 +17,9 @@ function SectionHeader(props) {
 	return (
 		<div id={props.section} className="mx-auto max-w-[80vw]">
 			<div data-aos="fade-up" className="flex flex-col pt-24">
-				<h1 className="font-cubano text-center text-4xl text-white sm:mb-5 sm:text-5xl">
+				<h1 className="font-black text-center text-4xl text-white sm:mb-5 sm:text-5xl">
 					{props.title}
-					<Hashtag href={sectionHref} />
+					<Suffix href={sectionHref} />
 				</h1>
 				<h2 className="text-center text-sm text-white sm:text-lg">
 					{props.desc}
