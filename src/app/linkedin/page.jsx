6abab893974d86d27a0dc76e81b61db58dce3263
Harman Singh <harman.singh@student.howest.be>
2024-08-06 20:03:06 +0200

    add '/linkedin' route

---
+++ src/app/linkedin/page.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const page = () => {
+	return (
+		<html>
+			<head>
+				<meta
+					http-equiv="refresh"
+					content="0; url=https://www.linkedin.com/in/harmanpnahal/"
+				/>
+			</head>
+		</html>
+	);
+};
+
+export default page;
