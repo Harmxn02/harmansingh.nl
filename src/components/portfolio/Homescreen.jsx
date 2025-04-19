d03ef7601e8dbfdaae9b007a621d494ee2128d0e
Harman Singh <harmandualsingh@gmail.com>
2025-04-19 16:51:47 +0200

    Add close button to Banner

--- src/components/portfolio/Homescreen.jsx
+++ src/components/portfolio/Homescreen.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 import Banner from "@/components/portfolio/Banner/Banner";
 import Intro from "@/components/portfolio/Intro/Intro";
@@ -12,12 +12,12 @@ import Contact from "./Contact/Form";
 
 import FooterSection from "./Footer/Footer";
 
-
-
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const Homescreen = () => {
+	const [showBanner, setShowBanner] = useState(true);
+
 	useEffect(() => {
 		AOS.init({
 			duration: 1000,
@@ -25,12 +25,19 @@ const Homescreen = () => {
 		});
 	}, []);
 
+	const handleCloseBanner = () => {
+		setShowBanner(false);
+	};
+
 	return (
 		<div className="App selection:bg-[#7e7dd680]">
-			<Banner
-				colour="gradient"
-				content="I'm available for SWE, Data Science & AI Internships 💪"
-			/>
+			{showBanner && (
+				<Banner
+					colour="gradient"
+					content="I'm available for SWE, Data Science & AI Internships 💪"
+					onClose={handleCloseBanner}
+				/>
+			)}
 			<Intro section="INTRO"></Intro>
 
 			<div id="BODY">
