8544d0db8087cfef5857a68e2358c6a854d4db92
Harman Singh <harmandualsingh@gmail.com>
2025-05-23 09:19:56 +0200

    FEAT: Move project data to a separate file and import it into Projects component

---
+++ src/app/data/projects.js
@@ -0,0 +1,85 @@
+const projects = [
+	{
+		id: 3,
+		highlighted: false,
+		name: "harmansingh.nl",
+		imageSrc: "https://placehold.co/600x400?text=Portfolio+Website",
+		imageAlt: "Preview of project.",
+		domain: "Frontend",
+		technologies: ["Next.js", "Tailwind CSS"],
+		description:
+			"My portfolio website was coded from scratch using Next.js and Tailwind CSS.",
+		repository: "https://github.com/Harmxn02/harmansingh.nl",
+		deployment: "https://www.harmansingh.nl",
+		button_text: ["Code Repository", "Live Deployment"],
+	},
+	// {
+	// 	id: 2,
+	// 	highlighted: false,
+	// 	name: "dev.harmansingh.nl",
+	// 	imageSrc: "https://placehold.co/600x400/?text=New+Portfolio+Website",
+	// 	imageAlt: "Preview of project.",
+	// 	domain: "Frontend",
+	// 	technologies: ["Next.js", "Tailwind CSS"],
+	// 	description:
+	// 		"The website you are currently looking at was also coded from scratch using Next.js and Tailwind CSS.",
+	// 	repository: "https://github.com/Harmxn02/dev.harmansingh.nl",
+	// 	deployment: "https://www.harmansingh.nl",
+	// 	button_text: ["Code Repository", "Live Deployment"],
+	// },
+	{
+		id: 4,
+		highlighted: false,
+		name: "links.harmansingh.nl",
+		imageSrc: "https://placehold.co/600x400?text=Links+Website",
+		imageAlt: "Preview of project.",
+		domain: "Frontend",
+		technologies: ["Next.js", "Tailwind CSS"],
+		description:
+			"A website that showcases all of my social media links in 1 place.",
+		repository: "https://github.com/Harmxn02/links.harmansingh.nl",
+		deployment: "https://links.harmansingh.nl",
+		button_text: ["Code Repository", "Live Deployment"],
+	},
+	{
+		id: 9,
+		highlighted: false,
+		name: "AI-driven Project Estimations Tool",
+		imageSrc: "https://placehold.co/600x400?text=AI+Project+Estimations",
+		imageAlt: "Preview of project",
+		domain: "ML/AI",
+		technologies: ["Python", "NLP", "RAG", "Streamlit", "Microsoft Azure"],
+		description:
+			"Solution to assist Delaware Consulting consultants in generating more accurate project estimations based on historical data",
+		repository: "https://github.com/Harmxn02/DELAWARExHOWEST",
+		button_text: ["Code Repository"],
+	},
+	{
+		id: 10,
+		highlighted: false,
+		name: "Agentic AI to automate ML tasks",
+		imageSrc: "https://placehold.co/600x400?text=Agentic+AI",
+		imageAlt: "Preview of project",
+		domain: "ML/AI",
+		technologies: ["Python", "Hugging Face", "LLM"],
+		description:
+			"Developed an agentic approach to Machine Learning that included autonomous communication and task delegation.",
+		repository: "https://github.com/Harmxn02/",
+		button_text: ["no code available"],
+	},
+	{
+		id: 11,
+		highlighted: false,
+		name: "Adversarial Attacks and Robustness",
+		imageSrc: "https://placehold.co/600x400?text=Adversarial+Attacks",
+		imageAlt: "Preview of project",
+		domain: "ML/AI",
+		technologies: ["Python", "PyTorch", "Computer Vision"],
+		description:
+			"Implemented and analysed the effectiveness of adversarial attacks on pre-trained computer vision models.",
+		repository: "https://github.com/Harmxn02/",
+		button_text: ["no code available"],
+	},
+];
+
+export default projects;
