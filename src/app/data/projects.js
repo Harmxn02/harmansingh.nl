2623d18c110c6a3fcc40e9d7f44ffaa0f1425c9a
Harman Singh <harmandualsingh@gmail.com>
2025-12-29 00:42:00 +0100

    UPDATE: Projects

--- src/app/data/projects.js
+++ src/app/data/projects.js
@@ -1,13 +1,13 @@
+// prettier-ignore
 const projects = [
 	{
 		// TODO: perhaps write a small whitepaper
 		visible: true,
 		name: "Consultancy Project Estimation Platform",
-		imageSrc: "https://placehold.co/600x400/?text=CPEP",
+		imageSrc: "https://placehold.co/600x400/?text=Project+Estimation+Platform",
 		domain: "ML/AI",
 		technologies: ["Python", "Streamlit", "Microsoft Azure"],
-		description:
-			"AI-driven solution to assist consultants in generating more accurate project estimations based on historical data",
+		description: "AI-driven solution to assist consultants in generating more accurate project estimations based on historical data",
 		left_button: "https://github.com/Harmxn02/DELAWARExHOWEST",
 		// right_button: "https://delawarexhowest.streamlit.app/",
 		button_text: ["Code Repository", "Live Deployment"],
@@ -19,20 +19,17 @@ const projects = [
 		imageSrc: "https://placehold.co/600x400/?text=Case study: AAR",
 		domain: "ML/AI",
 		technologies: ["Python", "PyTorch", "Computer Vision"],
-		description:
-			"Implemented and analysed the effectiveness of adversarial attacks on pre-trained computer vision models.",
+		description: "Implemented and analysed the effectiveness of adversarial attacks on pre-trained computer vision models.",
 		left_button: "https://github.com/Harmxn02/AIS-Project-TinyImageNet",
 		button_text: ["Code Repository"],
 	},
 	{
 		visible: false,
 		name: "H5 Research",
-		imageSrc:
-			"https://placehold.co/600x400/221a13/ffffff/?text=H5+Research&font=Lora",
+		imageSrc: "https://placehold.co/600x400/221a13/ffffff/?text=H5+Research&font=Lora",
 		domain: "Software Engineering",
 		technologies: ["Next.js", "Tailwind CSS", "Typescript"],
-		description:
-			"A website where I share all my research (currently in progress). Inspired by Open WebUI's homepage",
+		description: "A website where I share all my research (currently in progress). Inspired by Open WebUI's homepage",
 		left_button: "https://github.com/Harmxn02/h5.harmansingh.nl",
 		right_button: "https://h5.harmansingh.nl",
 		button_text: ["Code Repository", "Live Deployment"],
@@ -46,8 +43,19 @@ const projects = [
 		technologies: ["Javascript"],
 		description: "Browser extension that automatically redirects YouTube Shorts to the regular video player",
 		left_button: "https://github.com/Harmxn02/RedirectYouTubeShortsUI-Extension",
-		button_text: ["Code Repository"]
-	}
+		button_text: ["Code Repository"],
+	},
+	{
+		visible: true,
+		name: "Recreating AI Models from Scratch",
+		imageSrc: "https://placehold.co/600x400/001e2b/00ed64/?text=Recreating+AI+Models+from+Scratch&font=poppins",
+		domain: "ML/AI",
+		technologies: ["Python", "PyTorch"],
+		description: "... using research papers and architecture diagrams",
+		left_button: "https://github.com/Harmxn02/Recreating-AI-Models-from-Scratch",
+		// right_button: "https://github.com/Harmxn02/Recreating-AI-Models-from-Scratch/blob/master/report/main.pdf",
+		button_text: ["Code Repository"],
+	},
 ];
 
 export default projects;
